70af1105df5bb43d76e5a45192c4385fa988d400
AleVisual <pc_ale44@outlook.com>
2026-02-04 09:25:48 -0300

    Add comment to trigger GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Este comentario es para forzar un nuevo despliegue en GitHub Pages
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
